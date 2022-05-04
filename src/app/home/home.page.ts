8c15d3d3b13964fb5f90fd770b65129addb1d421
RasmusKjeldgaard <rkk@bankdata.dk>
2022-05-04 10:36:00 +0200

    Add capacitor with iOS and Android config

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage implements AfterViewInit {
+  public topInset;
+  
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+    this.topInset = getComputedStyle(document.documentElement).getPropertyValue(
+      "--ion-safe-area-top"
+    )}, 1000);
+  }
 }
