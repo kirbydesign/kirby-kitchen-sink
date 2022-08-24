f6426a2c6804f76a968f28b0d1f5b03bc4d30f56
Michael Hladky <michael@hladky.at>
2022-08-24 11:36:19 +0200

    fix: fix imports 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { KirbyModule } from '@kirbydesign/designsystem';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, KirbyModule],
   providers: [],
   bootstrap: [AppComponent],
 })
